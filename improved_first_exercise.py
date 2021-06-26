b5b7114610586d5bcfdc3aa3fe678374569eebf8
sabinadragoi <59471401+sabinadragoi@users.noreply.github.com>
2021-06-26 16:11:48 -0400

    Add files via upload

--- improved_first_exercise.py
+++ improved_first_exercise.py
@@ -11,22 +11,26 @@ import time
 
 start = time.time()
 
-def gen_sparse_X_ij(n,i,j):
+def gen_sparse_X_i(n,i): # X_i = I \tensor I ...\tensor X_i \tensor .. I
     if i==0:
-        X_ij= sparse.csc_matrix(np.array([[0, 1], [1, 0]]))
+        X_i= sparse.csc_matrix(np.array([[0, 1], [1, 0]]))
     else:
-        X_ij = sparse.identity(2)
+        X_i = sparse.identity(2)
     for k in range(1,n):
-        if k== i or k==j:
-            new_X_ij = sparse.csc_matrix(np.array([[0, 1], [1, 0]]))
+        if k== i:
+            new_X_i = sparse.csc_matrix(np.array([[0, 1], [1, 0]]))
         else:
-            new_X_ij = sparse.identity(2)
-        X_ij = scipy.sparse.kron(X_ij, new_X_ij)
+            new_X_i = sparse.identity(2)
+        X_i = scipy.sparse.kron(X_i, new_X_i)
 
-    return X_ij
+    return X_i
 
 
-def gen_sparse_H(n):
+def gen_sparse_X_ij(n,i,j): # X_iX_j
+    return np.multiply(gen_sparse_X_i(n,i),gen_sparse_X_i(n,j))
+
+
+def gen_sparse_H(n): # H = \sum_{i,j} X_iX_j
     # initializing empty Hamiltonian
     H = sparse.csc_matrix((2 ** n, 2 ** n), dtype=int)
 
@@ -38,25 +42,30 @@ def gen_sparse_H(n):
 
 
 def init_sparse_state(n,r):
-    if r==1:
+    if r==1: # |0^n\rangle state
         psi0= sparse.csc_matrix((np.array([1]), (np.array([0]), np.array([0]))), shape=(2**n, 1))
-    else:
+    else: # uniform superposition
         psi0 = (1 / np.sqrt(2 ** n)) * np.ones((2 ** n, 1))
 
     return psi0
 
+def init_boson_sparse_state(m,k):
+    # starts in pure H.O. state k
+    return sparse.csc_matrix((np.array([1]), (np.array([k]), np.array([0]))), shape=(m, 1))
 
-def time_ev_sparse_state(n,t,psi0):
+
+def time_ev_sparse_state(t,psi0,H):
+    # H = gen_sparse_H(n) or H = gen_sparse_H_coupled(m,n)
     # just to remember that we consider hbar =1
     hbar =1
 
-    psi_t= sparse.linalg.expm_multiply(-1j*t*gen_sparse_H(n)/hbar, psi0)
+    psi_t= sparse.linalg.expm_multiply(-1j*t*H/hbar, psi0)
 
     return psi_t
 
 
-def sparse_operator_average(n,t,psi0,op):
-    psi_t = time_ev_sparse_state(n,t,psi0)
+def sparse_operator_average(t,psi0,op,H):
+    psi_t = time_ev_sparse_state(t,psi0,H)
     return ((psi_t.conj().T @ (op @ psi_t)).real.toarray())[0][0]
 
 
@@ -68,7 +77,7 @@ def gen_sparse_overlap_op(n,r):
     return init_overlap_op
 
 
-def gen_sparse_Z_i(n,i):
+def gen_sparse_Z_i(n,i): # Z_i = I \tensor I ...\tensor Z_i \tensor .. I
     if i == 0:
         Z_i = sparse.csc_matrix(np.array([[1, 0], [0, -1]]))
     else:
@@ -90,7 +99,82 @@ def gen_sparse_Z_tot(n):
         Z_tot = np.add(Z_tot,gen_sparse_Z_i(n,i))
     return Z_tot
 
+def low_raise_op(m): # m = max number of bosons, a = lowering operator
+    a = sparse.csc_matrix((m+1, m+1))
+    a_dagger = sparse.csc_matrix((m+1, m+1))
+    for i in range(1, m+1):
+        # for lowering operator
+        data = np.array([np.sqrt(1 / i)])
+        row = np.array([i -1])
+        col = np.array([i])
+        add = sparse.csc_matrix((data, (row, col)), shape=(m+1, m+1))
+        a = np.add(a, add)
+
+        # for raising operator
+        row_dagger = np.array([i])
+        col_dagger = np.array([i-1])
+        add_dagger = sparse.csc_matrix((data, (row_dagger, col_dagger)), shape=(m+1, m+1))
+        a_dagger = np.add(a_dagger, add_dagger)
+
+
+    return (a,a_dagger)
+
+
+def gen_bosonic_mode(m): # m = max number of bosons
+    # define a+ a
+    return np.add(low_raise_op(m)[0],low_raise_op(m)[1])
+
+def coupled_init_state(m,k,n,r):
+    return sparse.kron(init_boson_sparse_state(m,k),init_sparse_state(n,r))
+
+
+def gen_sparse_sigma_i_pm(n,i): # sigma_i_plus/minus = I \tensor I ...\tensor sigma_plus/minus \tensor .. I
+    # shape should be (2**n, 2**n)
+    sigma_plus = np.array([[0, 1], [0, 0]])
+    sigma_minus = np.array([[0, 0], [1, 0]])
+
+    if i == 0:
+        sigma_i_plus = sparse.csc_matrix(sigma_plus)
+        sigma_i_minus = sparse.csc_matrix(sigma_minus)
+    else:
+        sigma_i_plus = sparse.identity(2)
+        sigma_i_minus = sparse.identity(2)
+    for k in range(1, n):
+        if k == i:
+            new_sigma_i_plus = sparse.csc_matrix(sigma_plus)
+            new_sigma_i_minus = sparse.csc_matrix(sigma_minus)
+        else:
+            new_sigma_i_plus = sparse.identity(2)
+            new_sigma_i_minus = sparse.identity(2)
+        sigma_i_minus = sparse.kron(sigma_i_minus, new_sigma_i_minus)
+        sigma_i_plus = sparse.kron(sigma_i_plus, new_sigma_i_plus)
+
+    return (sigma_i_minus, sigma_i_plus)
+
+# (sigma_i_minus, sigma_i_plus) = gen_sparse_sigma_i_pm(3, 1)
+# print(sigma_i_plus.shape)
+
+
+def gen_sparse_H_coupled(m,n):
+    # H = sum_i (a^{dagger}\sigma_i^- + a\sigma_i^+)
+    # a - raising/lowering operators acting on the bosonic mode
+    # \sigma - acts on the spins
+    (a,a_dagger)= low_raise_op(m)
+    H = sparse.csc_matrix((((2 ** n) * (m+1)), ((2 ** n) * (m+1))), dtype=int)
+
+    for i in range(n):
+        (sigma_i_minus, sigma_i_plus) = gen_sparse_sigma_i_pm(n, i)
+        add = np.add(sparse.kron(a_dagger,sigma_i_minus),sparse.kron(a,sigma_i_plus))
+        H = np.add(H, add)
+
+    return H
+
+
+
+
+
 ###################################################################
+# Averages of Z_tot and overlap operators for system of spins (without bosons)
 
 # def csv_init_write(filename, header):
 #     csvfile = open(filename, 'w')
@@ -108,80 +192,43 @@ def gen_sparse_Z_tot(n):
 #     while t <= 17:
 #         # Compute the result and write to the file
 #
-#         res_gen_overlap = sparse_operator_average(n,t,init_sparse_state(n,1),gen_sparse_overlap_op(n,1))
+#         res_gen_overlap = sparse_operator_average(t,init_sparse_state(n,1),gen_sparse_overlap_op(n,1),gen_sparse_H(n))
 #         writer_gen_overlap.writerow([n, t, res_gen_overlap])
 #
-#         res_gen_Z = sparse_operator_average(n, t, init_sparse_state(n, 1), gen_sparse_Z_tot(n))
+#         res_gen_Z = sparse_operator_average(t, init_sparse_state(n, 1), gen_sparse_Z_tot(n),gen_sparse_H(n))
 #         writer_gen_Z.writerow([n, t, res_gen_Z])
 #         t += 0.25
 #
 # gen_overlap_file.close()
 # gen_Z_file.close()
 
-##########################################
+#################################################
+# Average of average spin magnetization for coupled cavity
+
+# def csv_init_write(filename, header):
+#     csvfile = open(filename, 'w')
+#     writer = csv.writer(csvfile)
+#     writer.writerow(header)
+#     return writer, csvfile
+#
+# # Open the CSV files for writing
+# header = ["n", "t", "op_avg"]
+# writer_gen_Z, gen_Z_file = csv_init_write("gen_sparse_Z.csv", header)
+#
+# for n in range(3,11):
+#     t = 0
+#     while t <= 17:
+#         # Compute the result and write to the file
+#
+#         res_gen_Z = sparse_operator_average(t, init_sparse_state(n, 1), gen_sparse_Z_tot(n)/n, gen_sparse_H_coupled(1,n))
+#         writer_gen_Z.writerow([n, t, res_gen_Z])
+#         t += 0.25
+#
+# gen_Z_file.close()
+
+#####################################################
 
 
 end = time.time()
 print("time=", end - start)
 
-# data = "gen_overlap.csv" or data  = "gen_Z.csv"
-def spliting_data(n,data):
-    data_to_split = np.genfromtxt(data, delimiter=",", names=["x", "y", "z"])
-    times=[]
-    averages=[]
-    for i in range(1,len(data_to_split)):
-        if data_to_split[i][0]==n:
-            averages.append(data_to_split[i][2])
-            times.append(data_to_split[i][1])
-
-    return [np.array(times),np.array(averages)]
-
-def fit_sin(tt, yy):
-    '''Fit sin to the input time sequence, and return fitting parameters "amp", "omega", "phase", "offset", "freq", "period" and "fitfunc"'''
-    tt = np.array(tt)
-    yy = np.array(yy)
-    ff = np.fft.fftfreq(len(tt), (tt[1]-tt[0]))   # assume uniform spacing
-    Fyy = abs(np.fft.fft(yy))
-    guess_freq = abs(ff[np.argmax(Fyy[1:])+1])   # excluding the zero frequency "peak", which is related to offset
-    guess_amp = np.std(yy) * 2.**0.5
-    guess_offset = np.mean(yy)
-    guess = np.array([guess_amp, 2.*np.pi*guess_freq, 0., guess_offset])
-
-    def sinfunc(t, A, w, p, c):
-        return A * np.sin(w*t+p) + c
-
-    popt, pcov = scipy.optimize.curve_fit(sinfunc, tt, yy, p0=guess)
-    A, w, p, c = popt
-    f = w/(2.*np.pi)
-    fitfunc = lambda t: A * np.sin(w*t + p) + c
-
-    return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1./f, "fitfunc": fitfunc, "maxcov": np.max(pcov), "rawres": (guess,popt,pcov)}
-
-N=500
-times2= np.linspace(0, 17, 10*N)
-
-
-for n in range(3,11):
-    for data in ["gen_sparse_overlap.csv","gen_sparse_Z.csv"]:
-
-        [times, averages] = spliting_data(n, data)
-        res = fit_sin(times, averages)
-        # print("Amplitude=%(amp)s, Angular freq.=%(omega)s, phase=%(phase)s, offset=%(offset)s, Max. Cov.=%(maxcov)s" % res)
-
-
-        plt.plot(times2, res["fitfunc"](times2), "r-", label="fit curve", linewidth=1.5)
-
-        # plt.plot(times,averages,label="no fit", linewidth=1)
-        plt.scatter(times,averages)
-        plt.title('n='+ str(n))
-        plt.xlabel('time')
-
-        if data == "gen_sparse_overlap.csv":
-            operator = "overlap"
-            plt.ylabel('average of overlap_op')
-        else:
-            operator = "Z_tot"
-            plt.ylabel('average of Z_tot op')
-
-        plt.savefig('n='+ str(n)+', operator='+ operator)
-        # plt.show()
